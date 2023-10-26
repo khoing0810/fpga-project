e4af65e23dbe9b6d33f7d4e872ef2f7e1e30da0c
Nikhil Kandkur <nkandkur@berkeley.edu>
2023-10-25 21:44:22 -0700

    started regfile
    Co-authored-by: Khoi Nguyen <khoing0810@users.noreply.github.com>

--- hardware/src/riscv_core/reg_file.v
+++ hardware/src/riscv_core/reg_file.v
@@ -1,12 +1,34 @@
 module reg_file (
     input clk,
-    input we,
-    input [4:0] ra1, ra2, wa,
-    input [31:0] wd,
-    output [31:0] rd1, rd2
+    input we, // RegWEn
+    input [4:0] ra1, ra2, wa, //rs1, rs2, rd
+    input [31:0] wd, // data to write
+    output [31:0] rd1, rd2 //R[rs1], R[rs2]
 );
     parameter DEPTH = 32;
     reg [31:0] mem [0:31];
-    assign rd1 = 32'd0;
-    assign rd2 = 32'd0;
+    reg [31:0] rd1_reg;
+    reg [31:0] rd2_reg;
+    initial begin
+        // TODO: double check these on lab computers!
+        genvar i;
+        for (i = 0; i < DEPTH; i = i + 1) begin
+            mem[i] = 0;
+        end
+        rd1_reg = 0;
+        rd2_reg = 0;
+    end
+    assign rd1 = rd1_reg;
+    assign rd2 = rd2_reg;
+    always @(negedge clk) begin
+        //read
+        rd1_reg <= mem[ra1];
+        rd2_reg <= mem[ra2];
+    end
+    always @(posedge clk) begin
+        //write
+        if (we && wa != 5'd0) begin
+            mem[wa] <= wd;
+        end
+    end
 endmodule
