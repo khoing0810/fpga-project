feca6ecdda827c07c964c6bae8b09710780082af
Nikhil Kandkur <nkandkur@berkeley.edu>
2023-11-19 22:28:12 -0800

    fixed potential off-by-one in sampler

--- hardware/src/audio/sampler.v
+++ hardware/src/audio/sampler.v
@@ -7,21 +7,21 @@ module sampler (
     output pwm_out
 );
     // Remove these lines once you have implemented this module
-    reg [11:0] counter = 1;
+    reg [11:0] counter = 0;
     wire [9:0] dac_code;
     always @(posedge clk) begin
         if (rst) begin
-            counter <= 1;
+            counter <= 0;
         end 
         else begin
             counter <= counter + 1;
-            if (counter == 12'd2499) begin
+            if (counter == 12'd2500) begin
                 counter <= 0;
             end
         end  
     end
     
-    assign synth_ready = counter == 12'd2498;
+    assign synth_ready = counter == 12'd2499;
     assign dac_code = synth_valid ? scaled_synth_code : 10'd0;
     sigma_delta_dac sigma_delta_dac_inst (
         .clk(clk),
